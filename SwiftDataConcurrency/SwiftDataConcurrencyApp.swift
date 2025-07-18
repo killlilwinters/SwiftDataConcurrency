9004955b96375e7695d3a7e86c8598aad9b9d230
Maksym Horobets <makswinters@outlook.com>
2025-07-18 15:12:18 +0300

    Initial Commit

---
+++ SwiftDataConcurrency/SwiftDataConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataConcurrencyApp.swift
+//  SwiftDataConcurrency
+//
+//  Created by Maksym Horobets on 18.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
